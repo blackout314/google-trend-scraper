7e7f9f7173a228e9e67d4cfa88124822113ef5d3
cdenaro <carlo.denaro@lastminute.com>
2018-03-08 12:06:17 +0100

    first cleaned tool

--- gt_scraper_csv.py
+++ gt_scraper_csv.py
@@ -3,17 +3,14 @@ import time
 import os
 import shutil
 import copy
-import pandas as pd
 import re
 import csv
 import numpy as np
-from pandas import DataFrame
 import sys
 import json
 import urllib
 from datetime import datetime, timedelta
 import requests
-
 
 def get_buckets(start_date, end_date):
     start_date_dt = datetime.strptime(start_date, '%Y-%m-%d')
@@ -60,86 +57,38 @@ def get_csv(response_text):
     csv = requests.get('https://www.google.com/trends/api/widgetdata/multiline/csv?req={0}&token={1}&tz=-120'.format(request,token))
     return csv.text.encode('utf8')
 
-def parse_csv(csv_contents):
-    lines = csv_contents.split('\n')
-    df = pd.DataFrame(columns = ['date','value'])
-    dates = []
-    values = []
-    # Delete top 3 lines
-    for line in lines[3:-1]:
-        try:
-            dates.append(line.split(',')[0].replace(' ',''))
-            values.append(line.split(',')[1].replace(' ',''))
-        except:
-            pass
-    df['date'] = dates
-    df['value'] = values
-    return df   
-
-def get_daily_frames(start_date, end_date, keyword):
+def get_daily_frames(start_date, end_date, keyword, outputfile):
 
     bucket_list = get_buckets(start_date, end_date)
     frames = []
     for i in range(0,len(bucket_list) - 1):
         resp_text = get_data(bucket_list[i], bucket_list[i+1], keyword)
-        frames.append(parse_csv(get_csv(resp_text)))
+        frames.append((get_csv(resp_text)))
 
-    return frames
+    file = open(outputfile, 'w')
+    file.write(''.join(frames))
+    file.close()
 
-def get_weekly_frame(start_date, end_date, keyword):
+def get_weekly_frame(start_date, end_date, keyword, outputfile):
 
     if datetime.strptime(start_date, '%Y-%m-%d') > datetime.strptime(end_date, '%Y-%m-%d') - timedelta(days=180):
         print 'No need to stitch; your time interval is short enough. '
         return None
     else:
         resp_text = get_data(datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d'), keyword)
-        return parse_csv(get_csv(resp_text))
-
-def stitch_frames(daily_frames, weekly_frame):
-
-    daily_frame = pd.concat(daily_frames, ignore_index = True)
-
-    daily_frame.columns = ['Date', 'Daily_Volume']
-    pd.to_datetime(daily_frame['Date'])
-    
-    weekly_frame.columns = ['Week_Start_Date', 'Weekly_Volume']
-    daily_frame.index = daily_frame['Date']
-    weekly_frame.index = weekly_frame['Week_Start_Date']
-
-    bins = []
-
-    for i in range(0,len(weekly_frame)):
-        bins.append(pd.date_range(weekly_frame['Week_Start_Date'][i],periods=7,freq='d'))
-
-    final_data = {}
-
-    for i in range(0,len(bins)):
-        week_start_date = datetime.strftime(bins[i][0],'%Y-%m-%d')
-        for j in range(0,len(bins[i])):
-            this_date = datetime.strftime(bins[i][j],'%Y-%m-%d')
-            try:
-                this_val = int(float(weekly_frame['Weekly_Volume'][week_start_date])*float(daily_frame['Daily_Volume'][this_date])/float(daily_frame['Daily_Volume'][week_start_date]))
-                final_data[this_date] = this_val
-            except:
-                pass
-    final_data_frame = DataFrame.from_dict(final_data,orient='index').sort()
-    final_data_frame[0] = np.round(final_data_frame[0]/final_data_frame[0].max()*100,2)
-
-    final_data_frame.columns=['Volume']
-    final_data_frame.index.names = ['Date']
-
-    final_data_frame.to_csv('{0}.csv'.format(keywords.replace('+','')), sep=',')
-
+        file = open(outputfile, 'w')
+        file.write(get_csv(resp_text))
+        file.close()
 
 if __name__ == '__main__':
 
     start_date = sys.argv[1]
     end_date = sys.argv[2]
+    outputfile = sys.argv[3]
     geo = ''
     category = 22
-    keywords = '+'.join(sys.argv[3:])
+    keywords = '+'.join(sys.argv[4:])
 
-    daily_frames = get_daily_frames(start_date, end_date, keywords)
-    weekly_frame = get_weekly_frame(start_date, end_date, keywords)
+    daily_frames = get_daily_frames(start_date, end_date, keywords, outputfile)
+    # weekly_frame = get_weekly_frame(start_date, end_date, keywords, outputfile)
 
-    stitch_frames(daily_frames, weekly_frame)
